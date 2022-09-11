d1470b62799f412593e8fbe5ef482da3c4332926
Waldemir Rodrigues <waldemir.rodrigues88@gmail.com>
2022-09-11 15:38:10 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var listaFilmes = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPML32aUf3UmjG4GS9fJdAwZ1T0eZAOHPllQ&usqp=CAU","https://i0.wp.com/www.museumazzaropi.org.br/wp-content/uploads/2020/09/TristezaDoJeca-scaled.jpg?fit=1678%2C2560&ssl=1","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY3MF7UrJYAeOT347DJD7sJPXEnhFgi0jf5g&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMs1GmLMo-43spe0lCwOc40D97wWjQlUbTOQ&usqp=CAU",""];
+
+for (var i = 0; i < listaFilmes.length; i++){
+
+    document.write("<img src="+ listaFilmes[i] +">");
+}
